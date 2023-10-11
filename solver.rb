6132ee946d7a1ebb6b417ad4ebcff2a1b0783e76
WHB901 <abdulwhbhussain@gmail.com>
2023-10-11 19:24:43 +0500

    Add Method for fizzbuzz in ruby file

--- solver.rb
+++ solver.rb
@@ -2,4 +2,11 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if number % 15 == 0
+    return 'fizz' if number % 3 == 0
+    return 'buzz' if number % 5 == 0
+    number
+  end
 end
