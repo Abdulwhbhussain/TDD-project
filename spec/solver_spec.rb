360b5cf197bf7af0bf131bcee9132da5a6d2609f
WHB901 <abdulwhbhussain@gmail.com>
2023-10-11 19:18:45 +0500

    Add test case for reverse

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,13 @@ require_relative '../solver'
 require 'rspec'
 
 describe Solver do
+  before :all do
+    @solver = Solver.new
+  end
+
+  describe '#reverse' do
+    it 'returns reversed string' do
+      expect(@solver.reverse('abc')).to eq('cba')
+    end
+  end
 end
