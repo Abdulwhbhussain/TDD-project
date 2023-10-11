097644611c606e36c889643e98b9118985771848
WHB901 <abdulwhbhussain@gmail.com>
2023-10-11 19:22:44 +0500

    Add test case for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,4 +11,22 @@ describe Solver do
       expect(@solver.reverse('abc')).to eq('cba')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns fizz if number is divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'returns buzz if number is divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'returns fizzbuzz if number is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns number if number is not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(7)).to eq(7)
+    end
+  end
 end
